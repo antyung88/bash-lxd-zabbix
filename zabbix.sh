cb352ec9a90eb1ffe654ec61181bc7f4087d327b
Anthony Yung <99248117+antyung88@users.noreply.github.com>
2022-05-29 16:57:03 +0800

    Update zabbix.sh

--- zabbix.sh
+++ zabbix.sh
@@ -20,9 +20,7 @@ then
   exit 1
 fi
 
-# Check if snap is installed
-
-if ! command lxd &> /dev/null 2>&1 || { echo >&2 "LXD is not installed. Installing!" ; apt install snapd && snap install lxd; }
+if command -v lxd &> /dev/null 2>&1 || { echo >&2 "LXD/LXC not installed. Aborting!" ; exit 1; }
 then
 
 if ! command lxc info ${PROXY_CONTAINER} &> /dev/null 2>&1 || { echo >&2 "${PROXY_CONTAINER} container namespace exists. Aborting!" ; exit 1; }
@@ -45,50 +43,37 @@ lxc config device add ${PROXY_CONTAINER} myport443 proxy listen=tcp:0.0.0.0:443 
 
 # Run a script in the proxy container to pull in the required packages
 cat <<EOF| lxc exec ${PROXY_CONTAINER} bash
-
 echo "Updating Proxy Container"
 apt-get update
-
 echo "Installing Nginx"
 # Install Nginx
 apt-get install nginx -y
-
 cat <<\EOF_C >/etc/nginx/sites-available/zabbix
 server {
         listen 80 proxy_protocol;
         listen [::]:80 proxy_protocol;
-
         server_name ${HOST};
-
         location / {
                 proxy_set_header Host \$host;
                 proxy_set_header X-Real-IP \$remote_addr;
                 proxy_set_header X-Forwarded-Proto \$scheme;
                 proxy_pass http://${ZABBIX_CONTAINER}.lxd;
         }
-
         real_ip_header proxy_protocol;
         set_real_ip_from 127.0.0.1;
 }
 EOF_C
-
 rm /etc/nginx/sites-available/default
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/zabbix /etc/nginx/sites-enabled/
 systemctl restart nginx
-
 EOF
-
 # Run a script in the Zabbix container to pull in the required packages
-
 cat <<EOF| lxc exec ${ZABBIX_CONTAINER} bash
-
 echo "Updating Zabbix Container"
 apt-get update
-
 echo "Installing MariaDB"
 apt-get install mariadb-server mariadb-client -y
-
 echo "Configuring MariaDB"
 mysql <<EOF_C
 create database ${DB_NAME} character set utf8 collate utf8_bin;
@@ -96,16 +81,13 @@ create user '${DB_USER}'@'localhost' identified by '${DB_PASS}';
 grant all privileges on ${DB_NAME}.* to '${DB_USER}'@'localhost';
 flush privileges;
 EOF_C
-
 echo "Installing Zabbix"
 wget https://repo.zabbix.com/zabbix/5.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.2-1+ubuntu20.04_all.deb -P /tmp
 dpkg -i /tmp/zabbix-release_5.2-1+ubuntu20.04_all.deb
 apt-get update
 apt-get install zabbix-server-mysql zabbix-frontend-php zabbix-nginx-conf zabbix-agent -y
-
 echo "Create Zabbix Tables"
 zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -u${DB_USER} -p${DB_PASS} ${DB_NAME}
-
 echo "Configuring Nginx"
 rm /etc/zabbix/nginx.conf
 cat <<\EOF_C >/etc/zabbix/nginx.conf
@@ -113,46 +95,36 @@ server {
         listen 80;
         server_name ${HOST};
         root    /usr/share/zabbix;
-
         index   index.php;
-
         location = /favicon.ico {
                 log_not_found   off;
         }
-
         location / {
                 try_files       \$uri \$uri/ =404;
         }
-
         location /assets {
                 access_log      off;
                 expires         10d;
         }
-
         location ~ /\.ht {
                 deny            all;
         }
-
         location ~ /(api\/|conf[^\.]|include|locale|vendor) {
                 deny            all;
                 return          404;
         }
-
         location ~ [^/]\.php(/|$) {
                 fastcgi_pass    unix:/var/run/php/zabbix.sock;
                 fastcgi_split_path_info ^(.+\.php)(/.+)$;
                 fastcgi_index   index.php;
-
                 fastcgi_param   DOCUMENT_ROOT   /usr/share/zabbix;
                 fastcgi_param   SCRIPT_FILENAME /usr/share/zabbix\$fastcgi_script_name;
                 fastcgi_param   PATH_TRANSLATED /usr/share/zabbix\$fastcgi_script_name;
-
                 include fastcgi_params;
                 fastcgi_param   QUERY_STRING    \$query_string;
                 fastcgi_param   REQUEST_METHOD  \$request_method;
                 fastcgi_param   CONTENT_TYPE    \$content_type;
                 fastcgi_param   CONTENT_LENGTH  \$content_length;
-
                 fastcgi_intercept_errors        on;
                 fastcgi_ignore_client_abort     off;
                 fastcgi_connect_timeout         60;
@@ -165,25 +137,16 @@ server {
         }
 }
 EOF_C
-
 rm /etc/nginx/sites-available/default
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/zabbix/nginx.conf /etc/nginx/sites-enabled/zabbix
 systemctl restart zabbix-server zabbix-agent nginx php7.4-fpm
-
 EOF
-
 # Let's Encrypt
-
 echo 'Installing Certbot'
-
 cat <<EOF| lxc exec ${PROXY_CONTAINER} bash
-
 snap install certbot --classic
-
 echo 'Generating Certificates'
 certbot --nginx -d ${HOST} -m ${EMAIL} --agree-tos -n
-
 systemctl restart nginx
-
 EOF
